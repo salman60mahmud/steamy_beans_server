09edd5c13ec2c8dbcd4a42c0885bf9c6d0239564
salman60mahmud <salmanmahmud947.com>
2025-12-09 04:18:15 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -3,14 +3,46 @@ const app = express()
 var cors = require('cors')
 const port = process.env.PORT || 5000
 
+require('dotenv').config()
+
 // Middleware
 app.use(cors())
 app.use(express.json());
 
+
+// MongoDB connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g4g3fyf.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Steamy Beans app listening on port ${port}`)
+    console.log(`Steamy Beans app listening on port ${port}`)
+    
 })
