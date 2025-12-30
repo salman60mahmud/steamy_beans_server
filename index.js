a2b72fb605f0834fd31d1e943f69f1348ed2dcab
salman60mahmud <salmanmahmud947.com>
2025-12-30 20:21:53 +0600

    new user added

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const app = express()
-var cors = require('cors')
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+var cors = require('cors');
+require('dotenv').config();
 
-require('dotenv').config()
+const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 
 // MongoDB connection
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g4g3fyf.mongodb.net/?appName=Cluster0`;
 
@@ -28,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("SteamyBeans").collection("Users");
+
+    // add new user
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -39,10 +48,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Steamy Beans app listening on port ${port}`)
-    
+  console.log(`Steamy Beans app listening on port ${port}`)
 })
